02e4e5340b9994bb8b17a96d9912a123f1d8878d
Shreyash Kurne <shreyashkurne07@gmail.com>
2026-07-15 23:59:34 +0530

    Day 6: Upgraded with user input

---
+++ day06_temperatureConverter.py
@@ -0,0 +1,7 @@
+# Day 6: Interactive Temperature Converter
+
+celsius_text = input("Enter the temperature in Celsius: ")
+celsius = float(celsius_text)
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"🌡️ {celsius}°C is equal to {fahrenheit}°F")
