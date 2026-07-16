2d7494c286ac5b629fbaeef8160c41bb4dc5464a
Shreyash Kurne <shreyashkurne07@gmail.com>
2026-07-16 18:37:53 +0530

    Day 7: Even or Odd Checker

---
+++ day07_even_odd.py
@@ -0,0 +1,9 @@
+# Day 7: Even / Odd Checker
+
+user = input("Enter a whole number: ")
+number = int(user)
+
+if number % 2 == 0:
+    print(f"The number {number} is Even.")
+else:
+    print(f"The number {number} is Odd.")
